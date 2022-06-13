e01ace691af56356a7f0efd5ed4fb23b1386c328
shishnk <akorund@mail.ru>
2022-06-14 04:51:41 +0700

    Класс вектора

---
+++ courseProject/Vector.cs
@@ -0,0 +1,98 @@
+namespace courseProject;
+
+public class Vector<T> where T : INumber<T>
+{
+    private readonly T[] vec;
+    public int Length { get; init; }
+
+    public T this[int index]
+    {
+        get => vec[index];
+        set => vec[index] = value;
+    }
+
+    public Vector(int dim)
+    {
+        vec = new T[dim];
+        Length = dim;
+    }
+
+    public static T operator *(Vector<T> firstVec, Vector<T> secondVec)
+    {
+        T result = T.Zero;
+
+        for (int i = 0; i < firstVec.Length; i++)
+        {
+            result += firstVec[i] * secondVec[i];
+        }
+
+        return result;
+    }
+
+    public static Vector<T> operator *(double constant, Vector<T> vector)
+    {
+        Vector<T> result = new(vector.Length);
+
+        for (int i = 0; i < vector.Length; i++)
+        {
+            result.vec[i] = vector[i] * T.Create(constant);
+        }
+
+        return result;
+    }
+
+    public static Vector<T> operator +(Vector<T> firstVec, Vector<T> secondVec)
+    {
+        Vector<T> result = new(firstVec.Length);
+
+        for (int i = 0; i < firstVec.Length; i++)
+        {
+            result.vec[i] = firstVec[i] + secondVec[i];
+        }
+
+        return result;
+    }
+
+    public static Vector<T> operator -(Vector<T> firstVec, Vector<T> secondVec)
+    {
+        Vector<T> result = new(firstVec.Length);
+
+        for (int i = 0; i < firstVec.Length; i++)
+        {
+            result.vec[i] = firstVec[i] - secondVec[i];
+        }
+
+        return result;
+    }
+
+    public static void Copy(Vector<T> source, Vector<T> destination)
+    {
+        for (int i = 0; i < source.Length; i++)
+        {
+            destination[i] = source[i];
+        }
+    }
+
+    public void Fill(double value)
+    {
+        for (int i = 0; i < Length; i++)
+        {
+            vec[i] = T.Create(value);
+        }
+    }
+
+    public double Norm()
+    {
+        T result = T.Zero;
+
+        for (int i = 0; i < Length; i++)
+        {
+            result += vec[i] * vec[i];
+        }
+
+        return Math.Sqrt(Convert.ToDouble(result));
+    }
+
+    public ImmutableArray<T> ToImmutableArray()
+        => ImmutableArray.Create(vec);
+}
